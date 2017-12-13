e1b7f5fa7ef775022e4530a5a0a14dc22b845789
Amber Lahav <amberlahav@gmail.com>
2017-12-13 00:08:30 -0500

    added some final styling touches, before finalizing responsive

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -5,7 +5,36 @@ const portfolio = {};
 
 portfolio.init = function () {
 
-    
+    //Typing effect in header from typed.js
+    $(".typer").typed({
+        strings: ["developing", "designing", "discovering"],
+        stringsElement: null,
+        typeSpeed: 45,
+        startDelay: 1000,
+        backSpeed: 40,
+        backDelay: 1000,
+        loop: true,
+        loopCount: false,
+        showCursor: false,
+        cursorChar: ""
+    });
+    // setTimeout(function () {
+    //     $(".typer").typed({
+    //         strings: ["developing", "^222 designing", "^111 discovering"],
+    //         typeSpeed: 90,
+    //         startDelay: 0,
+    //         backSpeed: 65,
+    //         backDelay: 900,
+    //         loop: true,
+    //         loopCount: false,
+    //         showCursor: true,
+    //         cursorChar: "",
+    //         attr: null,
+    //         contentType: 'html',
+    //         callback: function () { }
+    //     })
+    // })
+
 
     $(".hamburger").click(function () {
         $(".navigation").toggleClass("open");
